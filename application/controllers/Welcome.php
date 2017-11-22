836c653a6c5f401cd02870ad60395c70f9b4b5ae
Ketan Prajapati <33022239+ketanmprajapati@users.noreply.github.com>
2017-11-22 12:21:32 +0530

    Update Welcome.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -7,7 +7,31 @@ class Welcome extends CI_Controller {
     public function __construct() {
         parent::__construct();
     }
-
+	
+	public function Process() {
+        extract($_POST);
+//        echo "<pre>";
+//        print_r($_POST);
+//        die;
+        $admin = $this->Data_model->Process();
+        if ($admin != null) {
+            $qry = $this->Data_model->Custome_query("select * from of_admin where of_admin_id='" . $_SESSION['id'] . "'");
+            if ($qry[0]['role'] == 'A') {
+                redirect(base_url() . 'Admin/AdminDashboard');
+            } else {
+                $con=array('of_admin_id'=>$_SESSION['id']);
+                $data=array(
+                    'update_date'=>date('Y-m-d H:i:s'),
+                );
+                $this->Data_model->Update_data('of_admin',$con,$data);
+                redirect(base_url() . 'Dashboard');
+            }
+        } else {
+            $sessdata = array('error' => "<strong>Error!</strong> Invalid Username or Password", 'errorcls' => "alert-danger");
+            $this->session->set_userdata($sessdata);
+            redirect(base_url() . $this->controll);
+        }
+    }
     public function index() {
         $data['party_name'] = $this->Data_Model->Custome_query("select * from reg where status !='B'");
         $this->load->view('Welcome', $data);
